9fa502e642ed834132d4723429c19def9633fe0f
Дмитро Сварковський <100209541+DmytroSvarkovskiy@users.noreply.github.com>
2022-12-13 22:09:42 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,41 +1,49 @@
 import PropTypes from 'prop-types';
 import { createPortal } from 'react-dom';
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { Overlay, ModalWindow } from './Modal.styled';
 const modalRoot = document.querySelector('#modal-root');
-export class Modal extends Component {
-  static propTypes = {
-    closeModal: PropTypes.func.isRequired,
-    dataImage: PropTypes.shape({
-      src: PropTypes.string.isRequired,
-      alt: PropTypes.string.isRequired,
-    }),
-  };
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleEscpClick);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleEscpClick);
-  }
-  handleEscpClick = e => {
+export const Modal = ({ dataImage: { src, alt }, closeModal }) => {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const handleEscpClick = e => {
+    console.log(e);
     if (e.code === 'Escape') {
-      this.props.closeModal();
+      closeModal();
     }
   };
-  onClick = e => {
+
+  useEffect(() => {
+    console.log('mount');
+    return console.log('unmount');
+  }, []);
+
+  // componentDidMount() {
+  //   window.addEventListener('keydown', this.handleEscpClick);
+  // }
+  // componentWillUnmount() {
+  //   window.removeEventListener('keydown', this.handleEscpClick);
+  // }
+
+  const onClick = e => {
     if (e.target === e.currentTarget) {
-      this.props.closeModal();
+      closeModal();
     }
   };
-  render() {
-    const { src, alt } = this.props.dataImage;
-    return createPortal(
-      <Overlay onClick={this.onClick}>
-        <ModalWindow>
-          <img src={src} alt={alt} />
-        </ModalWindow>
-      </Overlay>,
-      modalRoot
-    );
-  }
-}
+
+  return createPortal(
+    <Overlay onClick={onClick}>
+      <ModalWindow>
+        <img src={src} alt={alt} />
+      </ModalWindow>
+    </Overlay>,
+    modalRoot
+  );
+};
+
+Modal.propTypes = {
+  closeModal: PropTypes.func.isRequired,
+  dataImage: PropTypes.shape({
+    src: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired,
+  }),
+};
